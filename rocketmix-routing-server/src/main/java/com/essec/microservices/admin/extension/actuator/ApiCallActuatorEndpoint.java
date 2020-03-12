0e02a5742c763e21e108e055138ef5f00e824ef4
alex <depellegrin@essec.edu>
2020-03-12 18:56:35 +0100

    refine apicalls actuator endpoint

--- rocketmix-routing-server/src/main/java/com/essec/microservices/admin/extension/actuator/ApiCallActuatorEndpoint.java
+++ rocketmix-routing-server/src/main/java/com/essec/microservices/admin/extension/actuator/ApiCallActuatorEndpoint.java
@@ -1,9 +1,13 @@
 package com.essec.microservices.admin.extension.actuator;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.endpoint.web.annotation.RestControllerEndpoint;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,23 +24,40 @@ public class ApiCallActuatorEndpoint {
 	private ApiCallThroughputService throughputService;
 
 	@Autowired
-	private ApiCallSearchService searchService; 
+	private ApiCallSearchService searchService;
 
-	
-	@GetMapping("/throughput")
+	@GetMapping(path = "", produces = {MediaType.APPLICATION_JSON_VALUE})
+	public List<String> listEndpoints() {
+		List<String> result = new ArrayList<>();
+		for (Method aMethod : this.getClass().getMethods()) {
+			if (!Modifier.isPublic(aMethod.getModifiers())) {
+				continue;
+			}
+			if (aMethod.isAnnotationPresent(GetMapping.class)) {
+				GetMapping annotation = aMethod.getAnnotation(GetMapping.class);
+				for (String aPath : annotation.path()) {
+					result.add(aPath);
+				}
+			}
+		}
+		return result;
+	}
+
+	@GetMapping(path = "/throughput", produces = { MediaType.APPLICATION_JSON_VALUE })
 	public float throughput() {
 		return this.throughputService.getThroughputPerSecond();
 	}
-	
-	
-	@GetMapping("/findAll")
+
+	@GetMapping(path = "/findAll", produces = { MediaType.APPLICATION_JSON_VALUE })
 	public List<ApiCallEntry> findAll() {
 		return this.searchService.findAll();
 	}
-	
-	@GetMapping("/find/{keyword}")
+
+	@GetMapping(path = "/find/{keyword}", produces = { MediaType.APPLICATION_JSON_VALUE })
 	public List<ApiCallEntry> find(@PathVariable("keyword") String keyword) {
 		return this.searchService.performSearch(keyword);
 	}
+
 	
+
 }
